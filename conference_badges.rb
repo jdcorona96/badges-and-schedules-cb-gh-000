b590183d8a079783a69ea5eddeb0802ec52e9f47
Joseph Corona <josed.hc96@gmail.com>
2018-11-14 04:42:16 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,23 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(arr)
+  ret = []
+  arr.each do |name|
+    ret.push("Hello, my name is #{name}.")
+  end
+  ret
+end
+
+def assign_rooms(names)
+  ret = []
+  names.each_with_index do |name, index|
+    ret.push "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
+  ret
+end
+
+def printer
+  
+end
